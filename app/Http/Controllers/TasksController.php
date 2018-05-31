b830fd8a91b8a67ee975c2e04cb601d6d15b8a29
EC2 Default User <aajiro.kirameki@techacademy.jp>
2018-05-31 06:29:20 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,7 +47,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        
+        $this->validate($request, [
+            'status' => 'required|max:191',  
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+         $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
         
@@ -92,7 +99,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+          $this->validate($request, [
+            'status' => 'required|max:191', 
+            'content' => 'required|max:191',
+        ]);
+
+        
         $task = Task::find($id);
+             $task->status = $request->status;    // add
         $task->content = $request->content;
         $task->save();
         
