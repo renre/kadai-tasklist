1b61ec123de5b91ab07fc1f758b5f9c1b87356a9
EC2 Default User <aajiro.kirameki@techacademy.jp>
2018-05-31 01:46:51 +0000

    CRUD messages

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,9 +4,12 @@
 
     <h1>タスクリスト</h1>
 
- 
+ @if(count($tasks) > 0)
+ <ul>
              @foreach ($tasks as $task)
                 <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
             @endforeach
+            </ul>
+            @endif
             {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
 @endsection
