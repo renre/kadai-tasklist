dac731de1b03bedc41f0ab71dd769c3d128dcf8c
EC2 Default User <aajiro.kirameki@techacademy.jp>
2018-06-12 06:55:02 +0000

    finish2

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@
 
 
 Route::get('/', 'TasksController@index');
+ Route::resource('tasks', 'TasksController', ['only' => ['store','edit','update', 'destroy']]);
 
 
 Route::get('index', 'TasksController@index')->name('index.get');
-    Route::resource('tasks', 'TasksController', ['only' => ['store','edit','update', 'destroy']]);
-
+   
    
    Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -28,4 +28,5 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+     Route::resource('tasks', 'TasksController');
 });
