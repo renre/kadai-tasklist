72ff7cb0f39d9cda22e68504bda3fb1f389f2f55
EC2 Default User <aajiro.kirameki@techacademy.jp>
2018-06-14 00:49:22 +0000

    migration

--- database/migrations/2018_05_11_091809_add_content_to_tasks_table.php
+++ database/migrations/2018_05_11_091809_add_content_to_tasks_table.php
@@ -15,8 +15,13 @@ class AddContentToTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
                $table->increments('id');
+                 $table->integer('user_id')->unsigned()->index();
             $table->string('content');    // content add column
             $table->timestamps();
+             $table->string('status',10);
+              $table->foreign('user_id')->references('id')->on('users');
+            
+            
         });
     }
 
